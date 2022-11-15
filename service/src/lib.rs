10ba30668cfb041d1388505ad062ac6fb11ea6c3
MatsDK <67562518+MatsDK@users.noreply.github.com>
2022-11-15 19:50:55 +0100

    add serviceGenerator

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,17 +1,28 @@
 use proc_macro::{self, TokenStream};
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{format_ident, quote, ToTokens};
 use syn::{
     braced, parenthesized,
     parse::{Parse, ParseStream, Result},
-    parse_macro_input, FnArg, Ident, Token,
+    parse_macro_input,
+    token::Comma,
+    FnArg, Ident, PatType, ReturnType, Token,
 };
 
 struct Service {
     ident: Ident,
-    methods: Vec<Method>
+    methods: Vec<Method>,
+}
+
+struct Method {
+    ident: Ident,
+    output: ReturnType,
+    args: Vec<PatType>,
 }
 
 impl Parse for Service {
     fn parse(input: ParseStream) -> Result<Self> {
+        // input.parse::<Token![trait]>()?;
         <Token![trait]>::parse(input)?;
         let ident: Ident = input.parse()?;
 
@@ -20,19 +31,11 @@ impl Parse for Service {
 
         let mut methods = Vec::new();
         while !content.is_empty() {
-            methods.push(  content.parse::<Method>()?);
+            methods.push(<Method>::parse(&content)?);
         }
 
-        Ok(Service {
-            ident,
-            methods
-        })
+        Ok(Service { ident, methods })
     }
-}
-
-#[derive(Debug)]
-struct Method {
-    ident: Ident
 }
 
 impl Parse for Method {
@@ -45,10 +48,23 @@ impl Parse for Method {
         let content;
         parenthesized!(content in input);
 
+        let mut args = Vec::new();
+        for arg in content.parse_terminated::<FnArg, Comma>(FnArg::parse)? {
+            match arg {
+                FnArg::Typed(p) => args.push(p),
+                FnArg::Receiver(_) => {
+                    eprintln!("Not supported")
+                }
+            }
+        }
+
+        let output = input.parse()?;
         <Token![;]>::parse(input)?;
 
         Ok(Method {
-            ident
+            ident,
+            output,
+            args,
         })
     }
 }
@@ -56,7 +72,56 @@ impl Parse for Method {
 #[proc_macro_attribute]
 pub fn service(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let Service { ident, methods } = parse_macro_input!(item as Service);
-    println!("{:?}, {:?}", ident, methods);
 
-    TokenStream::new()
+    ServiceGenerator {
+        service_ident: ident,
+        methods,
+    }
+    .into_token_stream()
+    .into()
+}
+
+struct ServiceGenerator {
+    service_ident: Ident,
+    methods: Vec<Method>,
+}
+
+impl ServiceGenerator {
+    fn service_trait(&self) -> TokenStream2 {
+        let ServiceGenerator {
+            service_ident,
+            methods,
+            ..
+        } = self;
+
+        let types_and_fns = methods.iter().map(|Method { ident, output, .. }| {
+            quote! {
+                fn #ident(self, ctx: String) #output;
+            }
+        });
+
+        quote! {
+            trait #service_ident {
+                #( #types_and_fns )*
+            }
+        }
+    }
+
+    fn server_struct(&self) -> TokenStream2 {
+        let ServiceGenerator { service_ident, .. } = self;
+
+        let server_ident = format_ident!("{}Server", service_ident);
+
+        quote! {
+            struct #server_ident {
+
+            }
+        }
+    }
+}
+
+impl ToTokens for ServiceGenerator {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        tokens.extend(vec![self.service_trait(), self.server_struct()])
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use service::service;
-
-#[service]
+#[service::service]
 trait MyService {
     async fn send_message();
+
+    async fn send_event(a: String);
 }
 
 fn main() {}
